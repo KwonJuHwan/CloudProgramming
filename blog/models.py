b1a1d0ea08ae61310be067a506f00124f259aa53
201810900KwonJuHwan <kjh307ok@naver.com>
2023-04-04 15:31:28 +0900

    author, 작성자 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 import os.path
 
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
@@ -8,15 +10,18 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    #중복을 피하기위해 사용 -> upload_to
+    # 중복을 피하기 위해 사용 -> upload_to
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # 같이 삭제
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}' # 작성자 표시
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
