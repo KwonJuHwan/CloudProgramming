e838d1f7350e7624573e5e1efbf78da64a03bfb0
201810900KwonJuHwan <kjh307ok@naver.com>
2023-04-04 15:37:05 +0900

    Category model 추가 및 Slug

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    # URL 에 키워드 넣기 위해서 uuid 같은 숫자가 아닌, 텍스트로 노출 가능
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
